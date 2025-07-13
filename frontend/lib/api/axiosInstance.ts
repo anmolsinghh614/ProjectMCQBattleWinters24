212e1e179700266a49818aca15aa5f62b736df1d
anmol_singhh614 <anmol.singh.iitkgp@gmail.com>
2025-07-14 00:51:55 +0530

    bug fix

--- frontend/lib/api/axiosInstance.ts
+++ frontend/lib/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://projectmcqbattlewinters24.onrender.com',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://projectmcqbattlewinters24.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
